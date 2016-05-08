7b176dc3c606fd708769112e93dac23b1c13e0e0
Markus Opolka <markus@martialblog.de>
2016-05-08 21:22:35 +0200

    Added decorator for Mock repos... hope that works as expected. Otherwise I'll have to handle http-remote repos

--- tests/test_pullpush.py
+++ tests/test_pullpush.py
@@ -2,9 +2,54 @@
 
 
 import unittest
+import git
+import os
+import shutil
 
 import pullpush.pullpush as pp
 
+
+class mockrepos:
+    """
+    Initializes mock repos for the unittest.
+    """
+
+    tmp_dir = '/tmp/pullpush-unittest-repos/'
+
+    def __init__(self, function):
+        self.function = function
+
+    def __call__(self):
+
+        self.setup_mockrepo('source_repo')
+        self.setup_mockrepo('target_repo')
+
+        self.function(self)
+
+        self.remove_mockrepo('source_repo')
+        self.remove_mockrepo('target_repo')
+
+    def setup_mockrepo(self, reponame):
+        """
+        Creates a test repo in the /tmp dir and commits an empty file.
+        """
+
+        repo_dir = os.path.join(mockrepos.tmp_dir, reponame)
+        file_name = os.path.join(repo_dir, 'test-file')
+
+        repo = git.Repo.init(repo_dir)
+
+        open(file_name, 'wb').close()
+        repo.index.add([file_name])
+        repo.index.commit("Initial Commit")
+
+    def remove_mockrepo(self, reponame):
+        """
+        Removes a repository folder.
+        """
+
+        repo_dir = os.path.join(mockrepos.tmp_dir, reponame)
+        shutil.rmtree(repo_dir)
 
 class PullPush(unittest.TestCase):
     """Some basic tests to check the PullPush class"""
@@ -12,9 +57,11 @@ class PullPush(unittest.TestCase):
     def setUp(self):
         pass
 
+    @mockrepos
     def test_pull(self):
         assert True
 
+    @mockrepos
     def test_push(self):
         assert True
 
