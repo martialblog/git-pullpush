f7ab1e555e9bba0ffd4d685940d9e2ae24b999d8
Markus Opolka <markus@martialblog.de>
2016-05-08 20:28:48 +0200

    Boilerplated unittest... still needs to be implemented

---
+++ tests/test_pullpush.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+import unittest
+
+import pullpush.pullpush as pp
+
+
+class PullPush(unittest.TestCase):
+    """Some basic tests to check the PullPush class"""
+
+    def setUp(self):
+        pass
+
+    def test_pull(self):
+        assert True
+
+    def test_push(self):
+        assert True
+
+    def test_set_target_repo(self):
+        assert True
