ce6b9039fb992fbf32d263c1b96a23491c861a16
Markus Opolka <markus@martialblog.de>
2016-05-09 13:41:11 +0200

    mockrepo decorator more flexible

--- tests/test_pullpush.py
+++ tests/test_pullpush.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python3
 
-
+import tempfile
 import unittest
 import git
 import os
 import shutil
 
-import pullpush.pullpush as pp
+#import pullpush.pullpush as pp
 
 
-class mockrepos:
+class mockrepo:
     """
-    Initializes mock repos for the unittest.
+    Initializes a mock repo for the unittest.
     """
 
-    tmp_dir = '/tmp/pullpush-unittest-repos/'
+    TMP_DIR = tempfile.TemporaryDirectory(suffix='pullpush-unittest-repos')
 
-    def __init__(self, function):
+    def __init__(self, function, reponame):
         self.function = function
+        self.reponame = reponame
 
     def __call__(self):
 
-        self.setup_mockrepo('source_repo')
-        self.setup_mockrepo('target_repo')
-
+        self.setup_mockrepo(self.reponame)
         self.function(self)
-
-        self.remove_mockrepo('source_repo')
-        self.remove_mockrepo('target_repo')
+        self.remove_mockrepo(self.reponame)
 
     def setup_mockrepo(self, reponame):
         """
         Creates a test repo in the /tmp dir and commits an empty file.
         """
 
-        repo_dir = os.path.join(mockrepos.tmp_dir, reponame)
+        repo_dir = os.path.join(mockrepo.TMP_DIR, reponame)
         file_name = os.path.join(repo_dir, 'test-file')
 
         repo = git.Repo.init(repo_dir)
@@ -48,22 +45,24 @@ class mockrepos:
         Removes a repository folder.
         """
 
-        repo_dir = os.path.join(mockrepos.tmp_dir, reponame)
+        repo_dir = os.path.join(mockrepo.TMP_DIR, reponame)
         shutil.rmtree(repo_dir)
 
-class PullPush(unittest.TestCase):
+class PullPushTest(unittest.TestCase):
     """Some basic tests to check the PullPush class"""
 
     def setUp(self):
-        pass
+        pp = PullPush()
 
-    @mockrepos
+    #@mockrepo
     def test_pull(self):
         assert True
 
-    @mockrepos
+    #@mockrepo
     def test_push(self):
         assert True
 
+    @mockrepo('set_target_repo')
     def test_set_target_repo(self):
+        expected_repo_target = 'target'
         assert True
