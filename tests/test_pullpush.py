839937458c70b01441c9d62ac3a68569700a2065
Markus Opolka <markus@martialblog.de>
2016-05-11 18:58:05 +0200

    Rewritten mockrepo setup and teardown

--- tests/test_pullpush.py
+++ tests/test_pullpush.py
@@ -9,38 +9,38 @@ import shutil
 #import pullpush.pullpush as pp
 
 
-class mockrepo:
+class mockrepos:
     """
     Initializes a mock repo for the unittest.
     """
 
     TMP_DIR = tempfile.TemporaryDirectory(suffix='pullpush-unittest-repos')
 
-    def __init__(self, function, reponame):
+    def __init__(self, function, *repo):
         self.function = function
-        self.reponame = reponame
+        self.repos = repo
 
     def __call__(self):
 
-        self.setup_mockrepo(self.reponame)
+        self.setup_mockrepos()
         self.function(self)
-        self.remove_mockrepo(self.reponame)
+        self.teardown_mockrepos(self.reponame)
 
-    def setup_mockrepo(self, reponame):
+    def create_repo(self, reponame):
         """
         Creates a test repo in the /tmp dir and commits an empty file.
         """
 
         repo_dir = os.path.join(mockrepo.TMP_DIR, reponame)
         file_name = os.path.join(repo_dir, 'test-file')
-
         repo = git.Repo.init(repo_dir)
 
+        #TODO Maybe move to own funtion
         open(file_name, 'wb').close()
         repo.index.add([file_name])
         repo.index.commit("Initial Commit")
 
-    def remove_mockrepo(self, reponame):
+    def remove_repo(self, reponame):
         """
         Removes a repository folder.
         """
@@ -48,11 +48,24 @@ class mockrepo:
         repo_dir = os.path.join(mockrepo.TMP_DIR, reponame)
         shutil.rmtree(repo_dir)
 
+    def setup_mockrepos(self):
+        """
+        Creates all the desired repos.
+        """
+
+        for reponame in self.repos:
+            self.create_repo(reponame)
+
+    def teardown_mockrepos(self):
+
+        for reponame in self.repos:
+            self.remove_repo(reponame)
+
+
 class PullPushTest(unittest.TestCase):
     """Some basic tests to check the PullPush class"""
 
     def setUp(self):
-        pp = PullPush()
 
     #@mockrepo
     def test_pull(self):
@@ -64,5 +77,10 @@ class PullPushTest(unittest.TestCase):
 
     @mockrepo('set_target_repo')
     def test_set_target_repo(self):
+
+        tmp_dir = tempfile.TemporaryDirectory(prefix='pullpush-unittest-set_target_repo')
+        pp = PullPush(unittest_tmp_dir)
         expected_repo_target = 'target'
+        pp.set_target_repo(expected_repo_target)
+
         assert True
