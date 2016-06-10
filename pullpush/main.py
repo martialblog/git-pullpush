614c692d9e9b87d8743d9a48f8789c629ec67559
Markus Opolka <markus@martialblog.de>
2016-06-10 21:19:20 +0200

    Added Slack Integration
    Removed util.py

--- pullpush/main.py
+++ pullpush/main.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
-# TODO Slack Integration
 # TODO Retry-If-Fail implementation
 
 
 from tempfile import TemporaryDirectory
 from argparse import ArgumentParser
+from slacker import Slacker
 from pullpush import PullPush
 from sys import exit
+
 
 DESC = 'Pulls a git repository and pushes it somewhere'
 HELP_PULL = 'The repo to pull from'
 HELP_PUSH = 'The repo to push into'
+API_TOKEN = 'TOKEN'
+CHANNEL = '#puppet'
+
+Slack = Slacker(API_TOKEN)
+
+
+def notify(message, severity):
+    """
+    Wrapper for Slack notifications.
+    """
+
+    ICON = {
+        'INFO', ':page_with_curl:',
+        'WARN', ':warning:',
+        'ERROR', ':bangbang:',
+        '*', ':slack:'
+    }
+
+    msg = '{0} {1}'.format(ICON[severity], message)
+    Slack.chat.post_message(CHANNEL, msg)
 
 
 def main():
@@ -26,8 +47,13 @@ def main():
 
     with TemporaryDirectory() as temp_dir:
         pp = PullPush(repo_dir=temp_dir)
-        pp.pull(origin)
-        pp.push(target)
+        try:
+            pp.pull(origin)
+            pp.push(target)
+            notify(message="All's good", severity='INFO')
+        except Exception:
+            notify(message="Something is wrong", severity='ERROR')
+            exit(1)
 
 
 if __name__ == "__main__":
